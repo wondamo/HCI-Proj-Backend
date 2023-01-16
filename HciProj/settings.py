ccec509ff2609825668b00b1d82b592662a56b91
Wonder-David <wonderdavid2@gmail.com>
2023-01-16 19:33:49 +0100

    added hci-proj.onrender.com to allowed_hosts

--- HciProj/settings.py
+++ HciProj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a8on@7p=zgb&7fnzro(2bzx9%nz20sblvk$w@tz9@%s#l(+4a#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hci-proj.onrender.com']
 
 
 # Application definition
