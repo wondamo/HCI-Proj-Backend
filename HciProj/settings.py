9306f0a7a6dbaee0ab723aee2577cc313a52951b
Wonder-David <wonderdavid2@gmail.com>
2023-01-17 07:16:22 +0100

    finished configuration of corsheaders

--- HciProj/settings.py
+++ HciProj/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -56,6 +57,18 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:5174",
+]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://localhost:5174'
+)
+
+CSRF_TRUSTED_ORIGINS= [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://localhost:5174",
