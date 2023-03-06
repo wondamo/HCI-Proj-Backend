2a6021967fe104585e8c9bb2ff794fc3700ada4d
Wonder-David <wonderdavid2@gmail.com>
2023-03-06 16:48:58 +0100

    edited user active default value

--- staff/models.py
+++ staff/models.py
@@ -28,7 +28,7 @@ class Staff(AbstractBaseUser):
         max_length=255,
         unique=True
     )
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
